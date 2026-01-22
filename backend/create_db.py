4e858728355269d88aec1b8aaaae8b9fd13846a1
raheelsafelife <raheel.safelife@gmail.com>
2026-01-22 23:21:53 +0500

    Populate full CCU details for AWS synchronization

--- backend/create_db.py
+++ backend/create_db.py
@@ -90,27 +90,63 @@ if __name__ == "__main__":
             if not db.query(MCO).filter(MCO.name == name).first():
                 db.add(MCO(name=name, created_by="System"))
                 
-        # 3. Comprehensive CCU List
+        # 3. Comprehensive CCU List with Details
         list_ccus = [
-            'DuPage County CS Programs', 'Grundy County Health Department', 'Senior Services Ass. Elgin',
-            'Senior Services Ass. Aurora', 'Catholic Charities Of the Diocese of Joliet (Kankakee)',
-            'Senior Services Ass. Kendall', 'Senior Services of Will County', 'Lake County Senior Social Services (Lake)',
-            'Catholic Charities NWSS', 'Catholic Charities SSSS', 'CCSI Case Coordination, LLC (Area 5)',
-            'Catholic Charities SWSS', 'Premier Home Health Care (North)', 'Premier Home Health Care (South)',
-            'Catholic Charities OAS/NENW', 'CCSI Case Coordination, LLC (Area 6)', 'CCSI Case Coordination, LLC (Area 10)',
-            'CCSI Case Coordination, LLC (Area 8)', 'CCSI Case Coordination, LLC (Area 11)', 'CCSI Case Coordination, LLC (Area 12)',
-            'Solutions for Care', 'Aging Care Connections', 'Oak Park Township', 'Pathlights',
-            'Kenneth Young Center (Schaumburg)', 'North Shore Senior Center', 'Stickney Township Office on Aging',
-            'General CCU'
+            {'name': 'DuPage County CS Programs', 'address': '421 N. County Farm Rd., Wheaton, IL 60187', 'phone': '(630)407-6500', 'email': 'csprograms@dupagecounty.gov'},
+            {'name': 'Grundy County Health Department', 'address': '1320 Union St., Morris, IL 60450', 'phone': '(815)941-3400'},
+            {'name': 'Senior Services Ass. Elgin', 'address': '101 S. Grove Ave., Elgin, IL 60120', 'phone': '(847)741-0404', 'email': 'ssaiccu@seniorservicesassoc.org'},
+            {'name': 'Senior Services Ass. Aurora', 'address': '2111 Plum St. Suite 250, Aurora, IL 60506', 'phone': '(630)897-4035', 'email': 'ssaiccu@seniorservicesassoc.org'},
+            {'name': 'Catholic Charities Of the Diocese of Joliet (Kankakee)', 'address': '249 S. Schuyler Ave. #300, Kankakee, IL 60901', 'phone': '(815)932-1921'},
+            {'name': 'Senior Services Ass. Kendall', 'address': '908 Game Farm Rd., Yorkville, IL 60560', 'phone': '(630)553-5777', 'email': 'ssaiccu@seniorservicesassoc.org'},
+            {'name': 'Senior Services of Will County', 'address': '251 N Center St., Joliet, IL 60435', 'phone': '(815)740-4225', 'email': 'ccuwillco@willcountyseniors.org'},
+            {'name': 'Lake County Senior Social Services (Lake)', 'address': '116 N Lincoln Ave. Round Lake, IL 60073', 'phone': '(847)546-5733', 'email': 'cclakeccu@catholiccharities.net'},
+            {'name': 'Catholic Charities NWSS', 'address': '1801 W. Central Rd., Arlington Heights, IL 60005', 'phone': '(847)253-5500', 'email': 'infoccnw@catholiccharities.net'},
+            {'name': 'Catholic Charities SSSS', 'address': '15300 S. Lexington Ave., Harvey, IL 60426', 'phone': '(708)596-2222', 'email': 'ccssssccu@catholiccharities.net'},
+            {'name': 'CCSI Case Coordination, LLC (Area 5)', 'address': '329 W 18th St. Suite 801, Chicago, IL 60616', 'phone': '(312)726-1364', 'email': 'chicagoccu@ccsiccu.com'},
+            {'name': 'Catholic Charities SWSS', 'address': '2601 W. Marquette Ave, Chicago, IL 60629', 'phone': '(773)349-8092', 'email': 'intakesubarea7@catholiccharities'},
+            {'name': 'Premier Home Health Care (North)', 'address': '6321 N. Avondale Suite 101A, Chicago, IL 60631', 'phone': '(312)766-3361', 'email': 'premierccunorth@phhc.com'},
+            {'name': 'Premier Home Health Care (South)', 'address': '1081 S. Western Ave Suite LL 100, Chicago, IL 60643', 'phone': '(312)256-2900', 'email': 'PremierILCCU@phss.com'},
+            {'name': 'Catholic Charities OAS/NENW', 'address': '3125 N. Knox, Chicago, IL 60641', 'phone': '(773)583-9224', 'email': 'ccnenwccu@catholiccharities.net'},
+            {'name': 'CCSI Case Coordination, LLC (Area 6)', 'address': '310 S. Racine 8N, Chicago, IL 60607', 'phone': '(773)341-1790', 'email': 'chicagoccu@ccsiccu.com'},
+            {'name': 'CCSI Case Coordination, LLC (Area 10)', 'address': '310 S. Racine 8N, Chicago, IL 60607', 'phone': '(773)341-1790', 'email': 'ccsiarea10@ccsiccu.com'},
+            {'name': 'CCSI Case Coordination, LLC (Area 8)', 'address': '1000 E. 111th St. Suite 800, Chicago, IL 60628', 'phone': '(312)686-1515', 'email': 'ccsisoutheast@ccsiccu.com'},
+            {'name': 'CCSI Case Coordination, LLC (Area 11)', 'address': '1000 E. 111th St. Suite 800, Chicago, IL 60628', 'phone': '(312)686-1515', 'email': 'ccsisoutheast@ccsiccu.com'},
+            {'name': 'CCSI Case Coordination, LLC (Area 12)', 'address': '1000 E. 111th St. Suite 800, Chicago, IL 60628', 'phone': '(312)686-1515', 'email': 'ccsisoutheast@ccsiccu.com'},
+            {'name': 'Solutions for Care', 'address': '7222 W. Cermak Rd. Suite 200, Riverside, IL 60546', 'phone': '(708)447-2448', 'email': 'Info@solutionsforcare.org'},
+            {'name': 'Aging Care Connections', 'address': '111 W Harris Ave, La Grange, IL 60525', 'phone': '(708)354-1323', 'email': 'Info@agingcareconnection.org'},
+            {'name': 'Oak Park Township', 'address': '130 S. Oak Park Ave, 2nd Floor, Oak Park, IL 60302', 'phone': '(708)383-8060', 'email': 'ccureferrals@oakparktownship.org'},
+            {'name': 'Pathlights', 'address': '7808 West College Drive Suite 5E, Palos Heights, IL 60463', 'phone': '(708)361-0219', 'email': 'ldoa.tx@pathlights.org'},
+            {'name': 'Kenneth Young Center (Schaumburg)', 'address': '1001 Rohlwing Rd., Elk Grove Village, IL 60007', 'phone': '(847)524-8800'},
+            {'name': 'North Shore Senior Center', 'address': '161 Northfield Rd, IL 60061', 'phone': '(847)784-6040', 'email': 'mco@nssc.org'},
+            {'name': 'Stickney Township Office on Aging', 'address': '7745 S. Leamington Ave., Burbank, IL 60459', 'phone': '(708)636-8850', 'email': 'klivigni@townshipofstickney.org'},
+            {'name': 'General CCU'}
         ]
-        ccu_count = 0
-        for name in list_ccus:
-            if not db.query(CCU).filter(CCU.name == name).first():
-                db.add(CCU(name=name, created_by="System"))
-                ccu_count += 1
         
-        if ccu_count > 0:
-            print(f"  ✓ Seeded {ccu_count} new CCUs.")
+        ccu_added = 0
+        ccu_updated = 0
+        for data in list_ccus:
+            name = data['name']
+            existing = db.query(CCU).filter(CCU.name == name).first()
+            if not existing:
+                new_ccu = CCU(
+                    name=name,
+                    address=data.get('address'),
+                    phone=data.get('phone'),
+                    email=data.get('email'),
+                    created_by="System"
+                )
+                db.add(new_ccu)
+                ccu_added += 1
+            else:
+                # Update existing if details missing
+                changed = False
+                if data.get('address') and not existing.address: existing.address = data['address']; changed = True
+                if data.get('phone') and not existing.phone: existing.phone = data['phone']; changed = True
+                if data.get('email') and not existing.email: existing.email = data['email']; changed = True
+                if changed: ccu_updated += 1
+        
+        if ccu_added > 0 or ccu_updated > 0:
+            print(f"  ✓ CCUs: {ccu_added} added, {ccu_updated} updated with details.")
             
         db.commit()
         print("--- BASE DATA SEEDING COMPLETE ---")
